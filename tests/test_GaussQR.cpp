6cec76f8422caee14d882803dc2219220c9fd183
Anastasiia-Kuznetsova <kuznetsova.anastasiia@phystech.edu>
2024-02-26 14:26:43 +0300

    add gauss method

---
+++ tests/test_GaussQR.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+#include "../src/Solvers/GaussQR.hpp"
+
+
+TEST(GAUSS_QR, TEST_1) {
+    std::vector<std::vector<double>> a{{1, 2, 3}, {4, 5, 6}, {7, 8,7}};
+    std::vector<double> b{1,2,1};
+    DenseMatrix matrix(a);
+    std::vector ans = gauss_qr(matrix, b);
+    std::cout << ans;
+    std::vector<double> ans_{0.66666666666667 , -1.3333333333333333, 1};
+    for (std::size_t i = 0; i < ans_.size(); i ++) {
+        ASSERT_NEAR(ans[i], ans_[i], 1e-12);
+    }
+}
+
--- tests/test_householder.cpp
+++ tests/test_householder.cpp
@@ -2,17 +2,16 @@
 #include "../src/Decomposition/Householder.hpp"
 #include "../src/Matrix/DenseMatrix.hpp"
 
-TEST(DENSE, TEST_1) {
+TEST(HOUSEHOLDER, TEST_1) {
     std::vector<std::vector<double>> a{{1, 2, 3}, {4, 5, 6}, {7, 8,9}};
     std::vector<std::vector<double>> q{{-0.123091490979333, 0.904534033733291, 0.40824890463863}, {-0.492365963917331, 0.301511344577764, -0.816496580927726}, {-0.8616404368553292, -0.3015113445777631, 0.4082482904638634}};
-    std::vector<std::vector<double>> r{{-8.124038404635959, -9.601136296387955, -11.07823418813995}, {0, 9.045340337332926,1.809068067466585}, {0,0,0}};
+    std::vector<std::vector<double>> r{{-8.124038404635959, -9.601136296387955, -11.07823418813995}, {0, 0.9045340337332926,1.809068067466585}, {0,0,0}};
     DenseMatrix matrix(a);
     DenseMatrix Q(q);
     DenseMatrix R(r);
     std::pair<DenseMatrix<double>, DenseMatrix<double>> QR = householder(matrix);
     for(std::size_t i = 0; i < Q.get_height() * Q.get_width(); i++)
-        ASSERT_NEAR(QR.first.get_values()[0], Q.get_values()[0], 1e-12);
+        ASSERT_NEAR(QR.first.get_values()[i], Q.get_values()[i], 1e-6);
     for(std::size_t i = 0; i < R.get_height() * R.get_width(); i++)
-        ASSERT_NEAR(QR.second.get_values()[0], R.get_values()[0], 1e-12);
+        ASSERT_NEAR(QR.second.get_values()[i], R.get_values()[i], 1e-6);
 }
-
