ec353509e2fdb6a1ff17518317c24f38d506c87e
Anastasiia-Kuznetsova <kuznetsova.anastasiia@phystech.edu>
2024-04-07 10:12:21 +0300

    ускорение для ГС я так и не заставила сходится, где ошибка я не вижу, поэтому пока имеем, то что имеем ((, может, в ближайшем будущем починю

--- src/Solvers/Gauss-Seidel.hpp
+++ src/Solvers/Gauss-Seidel.hpp
@@ -24,34 +24,74 @@ std::vector<double> gauss_seidel( CSR<T>& matrix, const std::vector<T>& b, std::
 
 
 template<typename T>
-std::vector<double> symmetric_gauss_seidel( CSR<T>& matrix, const std::vector<T>& b, std::vector<T> x_0, double break_condition){
+std::vector<double> gauss_seidel_iter(CSR<T>& matrix, const std::vector<T>& b, std::vector<T> x_0){
     double tmp = 0;
+    T d = 0;
+    for(std::size_t i = 0; i < x_0.size(); i++){
+        std::size_t j = x_0.size() - 1 - i ;
+        tmp = 0;
+        for(std::size_t k = matrix.get_rows(j); k < matrix.get_rows(j + 1); k++){
+            if(matrix.get_cols(k) != j)
+                tmp += matrix.get_values(k) * x_0[matrix.get_cols(k)];
+            else d =  matrix.get_values(k);
+        }
+        x_0[j] = (b[j] - tmp) / d;
+    }
+
+    for(std::size_t j = 0; j < x_0.size(); j++){
+        tmp = 0;
+        for(std::size_t k = matrix.get_rows(j); k < matrix.get_rows(j + 1); k++){
+            if(matrix.get_cols(k) != j)
+                tmp += matrix.get_values(k) * x_0[matrix.get_cols(k)];
+            else d =  matrix.get_values(k);
+        }
+        x_0[j] = (b[j] - tmp) / d;
+    }
+    return x_0;
+}
+
+
+template<typename T>
+std::vector<double> symmetric_gauss_seidel( CSR<T>& matrix, const std::vector<T>& b, std::vector<T> x_0, double break_condition){
     double r_cur = 1e8;
     std::vector<double> res(x_0.size()); 
-    T d = 0;
     for(; r_cur > break_condition;){
-        for(std::size_t i = 0; i < x_0.size(); i++){
-            std::size_t j = x_0.size() - 1 - i ;
-            tmp = 0;
-            for(std::size_t k = matrix.get_rows(j); k < matrix.get_rows(j + 1); k++){
-                if(matrix.get_cols(k) != j)
-                    tmp += matrix.get_values(k) * x_0[matrix.get_cols(k)];
-                else d =  matrix.get_values(k);
-            }
-            x_0[j] = (b[j] - tmp) / d;
-        }
-
-        for(std::size_t j = 0; j < x_0.size(); j++){
-            tmp = 0;
-            for(std::size_t k = matrix.get_rows(j); k < matrix.get_rows(j + 1); k++){
-                if(matrix.get_cols(k) != j)
-                    tmp += matrix.get_values(k) * x_0[matrix.get_cols(k)];
-                else d =  matrix.get_values(k);
-            }
-            x_0[j] = (b[j] - tmp) / d;
-        }
+         x_0 = gauss_seidel_iter(matrix, b, x_0);
          res = (matrix * x_0) - b;
          r_cur = std::sqrt(dot(res, res));
     }
     return x_0;
+}
+
+
+template<typename T>
+std::vector<double> fast_gauss_seidel( CSR<T>& matrix, const std::vector<T>& b, std::vector<T> y_0, double rho, double break_condition){
+    double r_cur = 1e8;
+    std::vector<double> res(y_0.size()); 
+
+    double mu_0 = 1, mu_1 = 1 / rho;
+
+    std::vector<double> y_1 = gauss_seidel_iter(matrix, b, y_0);
+    res = (matrix * y_1) - b;
+    r_cur = std::sqrt(dot(res, res));
+
+    std::vector<T> y(y_0.size());
+    for(; r_cur > break_condition;){
+        y = 2 * mu_1 / rho * y_1 - mu_0 * y_0;
+        mu_0 = 2 / rho * mu_1 - mu_0;
+        y /= mu_0;
+
+        y_0 = y_1;
+        y_1 = y;
+
+        std::swap(mu_0, mu_1);
+
+
+        res = (matrix * y) - b;
+        r_cur = std::sqrt(dot(res, res));
+        std::cout << r_cur << std::endl;
+
+    }
+
+    return y_1;
 }
--- tests/test_GaussSeidel.cpp
+++ tests/test_GaussSeidel.cpp
@@ -1,6 +1,6 @@
 #include "gtest/gtest.h"
-#include "Solvers/Gauss-Seidel.hpp"
-
+#include "../src/Solvers/Gauss-Seidel.hpp"
+#include<../src/Solvers/Chebyshev.hpp>
 
 
 TEST(GAUSS_SEIDEL, TEST_1) {
@@ -15,14 +15,35 @@ TEST(GAUSS_SEIDEL, TEST_1) {
     }
 }
 
-TEST(SYMMETRIC_GAUSS_SEIDEL, TEST_1) {
-    std::vector<std::vector<double>> a{{12, 5,6}, {1,10, 8},{1,2,4}};
+TEST(SYMMETRIC_GAUSS_SEIDEL, TEST_2) {
+    std::vector<std::vector<double>> a{{10, 1, 0}, {1,7, 0.1},{0,0,1}};
     CSR mat(a);
     std::vector<double> x{0,0,0};
-    std::vector<double> b{7,4,1};
+    std::vector<double> b{20, 30, 1};
     std::vector<double>x_1 = symmetric_gauss_seidel(mat,b, x, 1e-15);
-    std::vector<double> ans{0.4461538461538462, 0.4076923076923077, -0.0653846153846154};
+    std::vector<double> ans{1.595652173913, 4.043478260869, 1.00000000000};
+    std::cout << ans << std::endl;
+    for (std::size_t i = 0; i < ans.size(); i ++) {
+        ASSERT_NEAR(ans[i], x_1[i], 1e-8);
+    }
+    std::cout << "end of sim GS" << std::endl;
+
+}
+
+TEST(FAST_GAUSS_SEIDEL, TEST_3) {
+    std::vector<std::vector<double>> a{{10, 1, 0}, {1,7, 0.1},{0,0,1}};
+    CSR mat(a);
+    std::vector<double> x{0,0,0};
+    std::vector<double> b{20, 30, 1};;
+    std::cout << "i'm there FGZ" << std::endl;
+
+    double lambda_max = calc_lambda_max(mat, 1e-8);
+    std::cout << lambda_max << std::endl;
+    std::vector<double>x_1 = fast_gauss_seidel(mat,b, x, lambda_max * 2, 1e-15);
+    std::cout << "i'm there" << std::endl;
+    std::vector<double> ans{1.595652173913, 4.043478260869, 1.00000000000};
     for (std::size_t i = 0; i < ans.size(); i ++) {
         ASSERT_NEAR(ans[i], x_1[i], 1e-8);
     }
 }
+
